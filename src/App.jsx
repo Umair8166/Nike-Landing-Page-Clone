500d39920f9cebd4187951cf96d268b3655a68e6
Umair Younas <umairyounas986@gmail.com>
2023-09-22 01:23:16 +0500

    small-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <main className="relative">
+        Nav
+        <section className="xl:padding-1 wide:padding-r padding-b">
+          Hero
+        </section>
+        <section className="padding">Popular Products</section>
+        <section className="padding">Super Quality</section>
+        <section className="padding-x py-10">services</section>
+        <section className="padding">Special Offer</section>
+        <section className="bg-pale-blue padding">Customer Reviews</section>
+        <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+        <section className="bg-black padding-x padding-y pb-8">
+          Copyright
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
